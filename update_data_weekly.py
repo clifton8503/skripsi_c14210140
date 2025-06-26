8a600193705ae7970bbf760cb7dacdee625310bc
clifton8503 <c14210140@john.petra.ac.id>
2025-06-27 03:06:13 +0700

    Delete update_data_weekly.py

--- update_data_weekly.py
+++
@@ -1,8 +0,0 @@
-# update_data.py
-from datetime import datetime
-from scrape_insert import update_data_to_db  # Ganti nama_file_import dengan nama script yang berisi fungsi
-
-update_data_to_db("weekly")   # Untuk harian
-# update_data_to_db("weekly") # Untuk mingguan (bisa buat file terpisah jika mau beda jadwal)
-with open("log_update.txt", "a") as f:
-    f.write(f"{datetime.now()}: Data berhasil diupdate\n")
